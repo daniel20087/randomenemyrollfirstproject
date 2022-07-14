dc26ad6e61e5f56693b90fab70ec59560aaea10b
daniel20087 <69530004+daniel20087@users.noreply.github.com>
2022-07-14 19:18:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp2 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int num1 = random.Next(1,21);
+            int num2 = random.Next(1,21);
+            Console.ReadLine();
+            Console.WriteLine("your number is " + num1);
+            Console.ReadLine();
+            Console.WriteLine("their number is " + num2);
+            if (num1 > num2)
+            {
+               Console.WriteLine("you win");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine("you lose");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
